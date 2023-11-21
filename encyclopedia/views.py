6a178fe435df67e952b7fd44afd52307a27ecefb
Proxtron <bratan_pavel@yahoo.com>
2023-11-21 08:10:11 -0800

    My first commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from markdown2 import Markdown
+from . import util
+
+
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+
