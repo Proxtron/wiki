9804747920621bf5bb484de7e1584728c97228c4
Proxtron <bratan_pavel@yahoo.com>
2023-12-02 13:28:08 -0800

    Finished added the rest of require functions

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("search", views.search, name="search")
+    path("search", views.search, name="search"),
+    path("newpage", views.newpage, name="newpage"),
+    path("createnewpage", views.createnewpage, name="createnewpage"),
+    path("editpage/<str:title>", views.editpage, name="editpage"),
+    path("editpagecontents/<str:title>", views.editpagecontents, name="editpagecontents"),
+    path("randompage", views.randompage, name="randompage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from markdown2 import Markdown
 from . import util
 from django.http import HttpResponse
+import os
+import random
 
 def convert_md_to_html(title):
     content = util.get_entry(title)
@@ -48,3 +50,64 @@ def search(request):
                     "possible_entries" : possible_entries,
                     "entry" : entry_search
                 })
+
+def newpage(request):
+    return render(request, "encyclopedia/newpage.html", {
+        
+    })
+
+def createnewpage(request):
+    if request.method == "POST":
+        entry_title = request.POST['title']
+        entry_text = request.POST['q']
+
+        for entry in util.list_entries():
+            if entry.lower() == entry_title.lower():
+                return HttpResponse("The entry with the title '%s' already exists" % entry_title)
+
+
+        current_directory = os.getcwd()
+        directory_name = os.path.join(current_directory, "entries", "%s.md" % entry_title)
+
+        entry_file = open(directory_name, 'w+')
+        entry_file.write(entry_text)
+        entry_file.close()
+        
+        return render(request, "encyclopedia\entry.html", {
+            "title" : entry_title,
+            "html_content" : entry_text
+        })
+
+def editpage(request, title):
+    entry_text = util.get_entry(title)
+    return render(request, "encyclopedia\editpage.html", {
+        "title" : title,
+        "entry_text" : entry_text
+    })
+
+def editpagecontents(request, title):
+    if request.method == "POST":
+        entry_text = request.POST["q"]
+
+        markdowner = Markdown()
+        html_content = markdowner.convert(entry_text)
+
+        current_directory = os.getcwd()
+        directory_name = os.path.join(current_directory, "entries", "%s.md" % title)
+
+        entry_file = open(directory_name, 'w+')
+        entry_file.write(entry_text)
+        entry_file.close()
+
+        return render(request, "encyclopedia\entry.html", {
+            "title" : title,
+            "html_content" : html_content
+        })
+
+def randompage(request):
+    random_entry = random.choice(util.list_entries())
+    html_content = convert_md_to_html(random_entry)
+    return render(request, "encyclopedia\entry.html", {
+        "title" : random_entry,
+        "html_content" : html_content
+    })
